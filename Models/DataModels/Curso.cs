658d1642233f27d85d57f2a1c8705ddaafc32fad
agome <agome@DESKTOP-81ER52L>
2022-09-22 15:07:55 -0300

    Add project files.

---
+++ Models/DataModels/Curso.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backendTest.Models.DataModels
+{
+    
+    public enum LEVEL
+    {
+        Basic,
+        Intermediate,
+        Advanced
+    }
+    
+    public class Curso : BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, StringLength(280)]
+        public string ShortDescription { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+        
+        [Required]
+        public LEVEL level { get; set; } = LEVEL.Basic;
+
+        [Required]
+        public ICollection<Category> Categories { get; set; } = new List<Category>();
+        public ICollection<Student> Students { get; set; } = new List<Student>();
+        
+        public Chapters Chapters { get; set; } = new Chapters();
+
+    }
+}
---
+++ Models/DataModels/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backendTest.Models.DataModels
+{
+    public class Student : BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, StringLength(100)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        public ICollection<Curso> Courses { get; set; } = new List<Curso>();
+
+    }
+}
