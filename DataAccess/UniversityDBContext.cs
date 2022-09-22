667cb4b984e89cc734dc8f27d0378f2f377f726a
agome <agome@DESKTOP-81ER52L>
2022-09-22 15:07:55 -0300

    Add project files.

---
+++ DataAccess/UniversityDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using backendTest.Models.DataModels;
+
+namespace backendTest.DataAccess
+{
+    public class UniversityDBContext : DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options) : base(options) 
+        {
+        
+        }
+        
+        //TODO: Add DbSets
+        public DbSet<User>? Users { get; set; }
+    }
+
+}
