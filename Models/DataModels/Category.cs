97aa3f174f8b74091a0ad1f5373277b7c3e8654d
agome <agome@DESKTOP-81ER52L>
2022-09-22 15:07:55 -0300

    Add project files.

---
+++ Models/DataModels/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backendTest.Models.DataModels
+{
+    public class Category : BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<Curso> Courses { get; set; } = new List<Curso>();
+    }
+}
